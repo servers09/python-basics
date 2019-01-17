4f5ccf624c341128b1d348e672ada0dd319cd99e
servers09 <46777258+servers09@users.noreply.github.com>
2019-01-17 17:04:48 +0800

    Create basic_operators.py

---
+++ basic_operators.py
@@ -0,0 +1,35 @@
+def int_Add(x,y):
+	result = x+y
+	return result
+
+def int_Sub(x,y):
+	result = x-y
+	return result
+
+def int_Multiply(x,y):
+	result = x*y
+	return result
+
+def int_Divide(x,y):
+	result = x/y
+	return result
+
+def int_Floor(x,y):
+	result = x//y
+	return result
+
+def int_Exponent(x,y):
+	result = x**y
+	return result
+
+def int_Modulo(x,y):
+	result = x%y
+	return result
+
+print(int_Add(3,5))
+print(int_Sub(3,5))
+print(int_Multiply(3,5))
+print(int_Divide(3,5))
+print(int_Floor(3,5))
+print(int_Exponent(3,5))
+print(int_Modulo(3,5))
