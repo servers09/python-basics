2f016934c5ebd4089773242db707fc8fb896221d
servers09 <46777258+servers09@users.noreply.github.com>
2019-01-17 17:30:48 +0800

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,16 @@
+def int(x):
+    return x
+
+def float(y):
+    return y
+
+def short(z):
+    return z
+
+def long(a):
+    return a
+
+print(int(214748364))
+print(float(1.554443))
+print(short(-32767))
+print(long(41514154))
