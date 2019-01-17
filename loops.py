c963b4f3b55d4f34b22436684253191b7707661f
servers09 <46777258+servers09@users.noreply.github.com>
2019-01-17 17:23:25 +0800

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,16 @@
+def loopwhile(n):
+    while True:
+        if n == 4:
+            return True
+            break
+        else:
+            return False
+
+def loopfor(b):
+    a=0
+    for i in b:
+        return bool(i)
+    a+=1
+
+print(loopwhile(0))
+print(loopfor([1]))
