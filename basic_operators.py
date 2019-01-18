2f5e49da1865e4441fabb3b7854de806eaf1a913
Vince Refamonte <46777258+servers09@users.noreply.github.com>
2019-01-18 09:25:42 +0800

    Update basic_operators.py

--- basic_operators.py
+++ basic_operators.py
@@ -26,6 +26,19 @@ def int_Modulo(x,y):
 	result = x%y
 	return result
 
+def int_MDAS(x,y,z):
+	result = x+y*z
+	return result
+
+def int_PEMDAS(x,y,z):
+	result = (x+y)*z
+	return result
+
+def float_convert(kilo):
+	result = float(kilo)*float(2.20462)
+	return result
+
+
 print(int_Add(3,5))
 print(int_Sub(3,5))
 print(int_Multiply(3,5))
@@ -33,3 +46,6 @@ print(int_Divide(3,5))
 print(int_Floor(3,5))
 print(int_Exponent(3,5))
 print(int_Modulo(3,5))
+print(int_MDAS(3,4,5))
+print(int_PEMDAS(3,4,5))
+print(float_convert(2))
