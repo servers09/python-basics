031efb13cc0638704012b20dba137fc65873a16e
Vince Refamonte <46777258+servers09@users.noreply.github.com>
2019-01-18 09:54:14 +0800

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -4,13 +4,42 @@ def int(x):
 def float(y):
     return y
 
-def short(z):
+def complex(z):
     return z
 
-def long(a):
-    return a
+def determine_type1(x):
+    return x
+
+def detemine_type2(y):
+    return y
+
+def determine_type3(z):
+    return z
+
+def add(x,y):
+    result = x+y
+    return result
+
+def subtract(x,y):
+    result = x-y
+    return result
+
+def multiply(x,y):
+    result = x*y
+    return result
+
+def divide(x,y):
+    result = x/y
+    return result
+
 
 print(int(214748364))
 print(float(1.554443))
-print(short(-32767))
-print(long(41514154))
+print(complex(100j))
+print(type(5))
+print(type(7.21144))
+print(type(7j))
+print(add(122,6j))
+print(subtract(4,52j))
+print(multiply(50.7,2j))
+print(divide(2j,250.4))
