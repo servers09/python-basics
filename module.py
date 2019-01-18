58fdc374dea2bd3474b8e44828de5a8a1c37a065
Vince Refamonte <46777258+servers09@users.noreply.github.com>
2019-01-18 13:10:36 +0800

    Create module.py

---
+++ module.py
@@ -0,0 +1,12 @@
+import mymodule
+
+mymodule.greeting("Jonathan") 
+mymodule.text("Vince")
+mymodule.print(mul(3))
+mymodule.print(div(3))
+mymodule.print(int_Floor(7,8))
+mymodule.print(int_Exponent(7,8))
+mymodule.print(int_Modulo(7,8))
+mymodule.print(int_MDAS(7,8,9))
+mymodule.print(int_PEMDAS(7,8,9))
+mymodule.print(float_convert(3))
