de03b86a39e96d93adb6acf0f40619b99cb55df2
Vince Refamonte <46777258+servers09@users.noreply.github.com>
2019-01-18 15:27:28 +0800

    Update classes_objects.py

--- classes_objects.py
+++ classes_objects.py
@@ -13,19 +13,40 @@ print(p.age)
 
 class compare:
     def less_than(self,num1,num2):
+
         if num1 < num2:
             return True
         else:
             return False
+    print(less_than("blank",3,5))
 
     def greater_than(self,num1,num2):
         if num1 > num2:
             return True
         else:
             return False
-        
+    print(greater_than("blank",3,5))
+
     def equal(self,num1,num2):
         if num1 == num2:
             return True
         else:
             return False
+    print(equal("blank",3,5))
+
+class odd_even_verify:
+    def __init__(self,num):
+        self.num = num
+    def bool_odd(self):
+        if self.num % 2 != 0:
+            return True
+        else:
+            return False
+    def bool_even(self):
+        if self.num % 2 == 0:
+            return True
+        else:
+            return False
+odd_even = odd_even_verify(4)
+print(odd_even.bool_odd())
+print(odd_even.bool_even())
