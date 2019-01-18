4cdd10cc5f3c1b00e62e574a1dee5746169672ed
Vince Refamonte <46777258+servers09@users.noreply.github.com>
2019-01-18 09:27:25 +0800

    Update lists.py

--- lists.py
+++ lists.py
@@ -48,73 +48,6 @@ def check():
 	if "jerky" in food:
 		return str("Match Found")
 
-def length(n):
-	return str("The length of the list is " + str(len(n)))
-
-def multipleset():
-	n = [((1, 2), (3,4)),(1, 2, 3)]
-	return str(n)
-	y = n[0]
-	return str(y)
-	x = y[0]
-	return str(x)
-	z = x[1]
-	return str(z)
-
-def intersection(a, b):
-	a.intersection_update(b)
-	return str(a)
-
-def difference(a, b):
-	c = b.difference(a)
-	return str(c)
-
-def symmetric(a, b):
-	c = a.symmetric_difference(b)
-	sorted(c)
-	return str(c)
-
-def unions():
-	a = {1, 2, 3}
-	b = {4, 5, 6}
-	c = {7, 8, 9}
-	d = {10, 11, 12}
-	e = a.union(b, c, d)
-	return str(e)
-
-def extends():
-	b = ["True", "False"]
-	a = [1, 0]
-	b.extend(a)
-	return str(b)
-
-def whileloop():
-	a = [1, 2, 3, 4, 5]
-	i = 0
-	while i<5:
-		i+=2
-		print(i)
-
-def sorting(x):
-	x.append(7)
-	x.sort()
-	return str(x)
-
-def forloop():
-	a = [1, 2, 3, 4]
-	i = 0
-	for i in a:
-		print(i)
-		i+=1
-
-
-
-
-
-
-
-
-
 
 print(add())
 print(remove())
@@ -126,13 +59,3 @@ print(counting([1, 2, 3, 4, 5, 6, 7, 8]))
 print(deleting_values(["Philippines", "Japan", "USA", "Russia", "China"]))
 looping()
 print(check())
-print(length([1, 2, 3, 4, 5, 6]))
-print(multipleset())
-print(intersection({"a", "b", "c", "d"}, {"a", "d", "f"}))
-print(difference({"a", "b", "c", "d"}, {"a", "d", "f"}))
-print(symmetric({"a", "b", "c", "d"}, {"a", "d", "f"}))
-print(unions())
-print(extends())
-whileloop()
-print(sorting([8, 4, 5, 6]))
-forloop()
